51eba511b1ff05d3b7a58cd4da1fea338cbf4298
kuhasun <kuhasun@live.com>
2018-09-11 23:21:35 +0700

    Notification AND Update Datafile

--- app/src/main/java/com/example/kuhas/smartfarm_04/page/NotificationService.java
+++ app/src/main/java/com/example/kuhas/smartfarm_04/page/NotificationService.java
@@ -8,6 +8,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
 import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -63,15 +64,14 @@ public class NotificationService extends Service {
 
     private void showNotificationAlert() {
 
-        NotificationManager mNotificationManager =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-
+        NotificationManagerCompat mNotificationManager = NotificationManagerCompat.from(this);
 // Check Version Android
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
             NotificationChannel channel = new NotificationChannel("default", "YOUR_CHANNEL_NAME",
                     NotificationManager.IMPORTANCE_DEFAULT);
             channel.setDescription("YOUR_NOTIFICATION_CHANNEL_DISCRIPTION");
-            mNotificationManager.createNotificationChannel(channel);
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
 
 
             NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(getApplicationContext(), "default")
